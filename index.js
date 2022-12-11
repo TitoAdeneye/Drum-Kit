6cf8d1c3da13f2a9c77009c72c6d704af6c3a9c3
TitoAdeneye <109716303+TitoAdeneye@users.noreply.github.com>
2022-12-11 09:32:09 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
-var numberOfDrums = document.querySelectorAll(".drum").length;
+const numberOfDrums = document.querySelectorAll(".drum").length;
 
-for (var i = 0; i<numberOfDrums; i++) {
+for (let i = 0; i<numberOfDrums; i++) {
     document.querySelectorAll(".drum")[i].addEventListener("click", function(){
 
-    var buttonInnerHTMl = this.innerHTML;
+    const buttonInnerHTMl = this.innerHTML;
 
     buttonSound(buttonInnerHTMl);
 
@@ -22,37 +22,37 @@ for (var i = 0; i<numberOfDrums; i++) {
 
     switch (key) {
         case "w":
-            var tom1 = new Audio("sounds/tom-1.mp3");
+            const tom1 = new Audio("sounds/tom-1.mp3");
             tom1.play();
             break;
 
         case "a":
-            var kickBass = new Audio("sounds/kick-bass.mp3");
+            const kickBass = new Audio("sounds/kick-bass.mp3");
             kickBass.play();
         break;
 
         case "s":
-            var snare = new Audio("sounds/snare.mp3");
+            const snare = new Audio("sounds/snare.mp3");
             snare.play();
         break;
 
         case "d":
-            var crash = new Audio("sounds/crash.mp3");
+            const crash = new Audio("sounds/crash.mp3");
             crash.play();
         break;
 
         case "j":
-            var tom2 = new Audio("sounds/tom-2.mp3");
+            const tom2 = new Audio("sounds/tom-2.mp3");
             tom2.play();
         break;
 
         case "k":
-            var tom3 = new Audio("sounds/tom-3.mp3");
+            const tom3 = new Audio("sounds/tom-3.mp3");
             tom3.play();
         break;
 
         case "l":
-            var tom4 = new Audio("sounds/tom-4.mp3");
+            const tom4 = new Audio("sounds/tom-4.mp3");
             tom4.play();
         break;
 
@@ -63,7 +63,7 @@ for (var i = 0; i<numberOfDrums; i++) {
 
 function buttonAnimation(currentKey){
 
-    var activeKey = document.querySelector("." + currentKey);
+    const activeKey = document.querySelector("." + currentKey);
 
     activeKey.classList.add("pressed");
 
